e9dae69d069d12625b209e5b5fdc3da1bc5b1f56
tbressel <tbressel.dev@gmail.com>
2024-02-01 15:52:29 +0100

    First deposit of Crud App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+////////////////////////////////////////////////////////
+//////////////////   IMPORTATIONS   ////////////////////
+////////////////////////////////////////////////////////
 
-function App() {
+// Style importations
+import { Reset } from 'styled-reset';
+
+
+// React importations
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Context importation
+
+// Types importation
+
+// Components importation
+import Home from './pages/Home';
+
+////////////////////////////////////////////////////////////
+//////////////////   STYLE COMPONENTS   ////////////////////
+////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////
+//////////////////   MAIN COMPONENT   //////////////////////
+////////////////////////////////////////////////////////////
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+            <BrowserRouter> 
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </BrowserRouter>
+    </>
   );
 }
-
 export default App;
